5534efa972c5c7de9ad9e95808c8f786419c9545
lazarowend <lazaroclans1@gmail.com>
2024-11-14 11:48:02 -0300

    add ClassBaseView in movies

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . views import CreateListMovie, DetailUpdateDeleteMovie
+from . views import ListCreateMovieView, RetrieveUpdateDestroyMovieView
 
 
 urlpatterns = [
-   path('movies/', CreateListMovie.as_view(), name='create_list_movie'),
-   path('movies/<int:pk>/', DetailUpdateDeleteMovie.as_view(), name='detail_update_delete_movie'),
+   path('movies/', ListCreateMovieView.as_view(), name='create_list_movie'),
+   path('movies/<int:pk>/', RetrieveUpdateDestroyMovieView.as_view(), name='detail_update_delete_movie'),
 ]
